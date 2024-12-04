924baae02786203a53aa29978fab0ab31465644f
Pinaraltinkaya45 <189837954+Pinaraltinkaya45@users.noreply.github.com>
2024-12-04 17:59:28 +0300

    Add project files.

---
+++ Erisim_Belirleyicileri/Ogrenci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erisim_Belirleyicileri
+{
+    public class Ogrenci
+    {
+        private int id = 1;
+        private string isim = "Enes";
+        public string soyisim = "Bayram";
+        public string okul = "Doğuş Üniversitesi";
+
+        public void adSoyadYazdir()
+        {
+            Console.WriteLine("Adı: " + isim + "Soyadi: " + soyisim);
+        }
+       
+        public void okulYazdir()
+        {
+            Console.WriteLine("Okul İsmi: " + okul);
+        }
+
+    }
+}
---
+++ Erisim_Belirleyicileri/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erisim_Belirleyicileri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Private: sahip olduğu class içinde erişilebilen, sadece bulunduğu classa özgüdür
+            // Public: hiç bir kısıtlaması olmayan her yerden erişmemize olanak sağlayan erişim belirleyicisidir
+          
+
+            Ogrenci ogrenci = new Ogrenci();
+
+            Console.WriteLine(ogrenci.okul);
+            Console.WriteLine(ogrenci.soyisim);
+            ogrenci.okulYazdir();
+
+            Console.ReadLine();
+        }
+    }
+}
